1e98affefe741779798c31772305f98425c3b189
Jimin Byun <jimin.stella@gmail.com>
2019-08-31 19:02:02 -0700

    allow connection from react

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Articles from "../components/Article";
+import CustomForm from "../components/Form";
 
 class ArticleList extends React.Component {
     state = {
@@ -16,7 +17,18 @@ class ArticleList extends React.Component {
         });
     }
     render() {
-        return <Articles data={this.state.articles} />;
+        return (
+            <div>
+                <Articles data={this.state.articles} />
+                <br />
+                <h2>Create an article</h2>
+                <CustomForm
+                    requestType="post"
+                    articleID={null}
+                    btnText="Create"
+                />
+            </div>
+        );
     }
 }
 
